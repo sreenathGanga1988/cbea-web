d07a657d5472e1043220baf9dcc6bedbf21b066e
sreenath.sreeganga@gmail.com <sreenathganga@hotmail.com>
2025-01-02 10:50:19 +0530

    environmnet changed

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
 
  //  BaseUrl: "https://www.cbeugjfws.co.in/api";
- BaseUrl:  "  http://localhost:5293/api",
- //BaseUrl: "  http://sreenathganga-001-site7.jtempurl.com/api",
+ //BaseUrl:  "  http://localhost:5293/api",
+ BaseUrl: "  http://sreenathganga-001-site7.jtempurl.com/api",
 
 
 };
